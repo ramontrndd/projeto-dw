494218fa4bcf041dd1ad4b5823aaa6b681713f02
ramontrndd <134494678+ramontrndd@users.noreply.github.com>
2024-10-18 20:42:39 -0300

    refactor:  component imports and file structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "../components/header/header.component";
-import { NavbarComponent } from "../components/navbar/navbar.component";
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeaderComponent } from './components/header/header.component';
+
 
 
 
