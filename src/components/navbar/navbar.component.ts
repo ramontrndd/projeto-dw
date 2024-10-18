3638d506bfe9da7c6b4b77e7b065d4b90fcf2513
ramontrndd <134494678+ramontrndd@users.noreply.github.com>
2024-10-18 19:27:00 -0300

    feat: add navbar component

--- src/components/navbar/navbar.component.ts
+++ src/components/navbar/navbar.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbar, MatToolbarRow } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [MatToolbar,MatButtonModule, MatToolbarRow],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
